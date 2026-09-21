e9ffb837a1c2e9b2f0bd47a7cbecd19c1ff8b802
semanard8-prog <semanard8@gmail.com>
2026-09-20 22:50:24 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
    Bump CACHE on every deploy so phones pick the new build up.
    ============================================================ */
 
-var CACHE = 'airlife-pcg-v7-codebag';
+var CACHE = 'airlife-pcg-v10-dispatch-vent';
 var FONTS = 'airlife-fonts-v1';
 
 var SHELL = [
